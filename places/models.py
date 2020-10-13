a8c24a1518b596da9da8f70911a31928da543923
AlexanderLyaksa <sanchos2004@gmail.com>
2020-10-13 15:00:13 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -10,3 +10,11 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE
+    )
+    picture = models.ImageField(upload_to='static/jpeg')
