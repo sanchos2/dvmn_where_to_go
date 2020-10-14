dabb54331ad7ddd9bc4f3066e6623166d21d55a2
AlexanderLyaksa <sanchos2004@gmail.com>
2020-10-14 10:45:41 +0300

    Refactor code

--- places/views.py
+++ places/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Place
+
+
+def index(request):
+    places = Place.objects.all()
+    data = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for place in places:
+        data["features"].append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.lng, place.lat]
+                    },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.id,
+                    "detailsUrl": "./static/places/moscow_legends.json"
+                    }
+            }
+        )
+    context = {'data': data}
+    return render(request, 'index.html', context=context)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import include, path
 
-
-from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', include('places.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
