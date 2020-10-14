ad80fb231a7934f5060ec23be9141e3b51ddf964
AlexanderLyaksa <sanchos2004@gmail.com>
2020-10-14 10:11:33 +0300

    Makes GeoJSON from database

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,35 +1,27 @@
 from django.shortcuts import render
+from places.models import Place
 
 
 def index(request):
+    places = Place.objects.all()
     data = {
-          "type": "FeatureCollection",
-          "features": [
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for place in places:
+        data["features"].append(
             {
-              "type": "Feature",
-              "geometry": {
-                "type": "Point",
-                "coordinates": [37.62, 55.793676]
-              },
-              "properties": {
-                "title": "«Легенды Москвы",
-                "placeId": "moscow_legends",
-                "detailsUrl": "./static/places/moscow_legends.json"
-              }
-            },
-            {
-              "type": "Feature",
-              "geometry": {
-                "type": "Point",
-                "coordinates": [37.64, 55.753676]
-              },
-              "properties": {
-                "title": "Крыши24.рф",
-                "placeId": "roofs24",
-                "detailsUrl": "./static/places/roofs24.json"
-              }
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.lng, place.lat]
+                    },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.id,
+                    "detailsUrl": "./static/places/moscow_legends.json"
+                    }
             }
-          ]
-        }
+        )
     context = {'data': data}
     return render(request, 'index.html', context=context)
