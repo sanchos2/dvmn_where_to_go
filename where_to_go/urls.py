857da8cdd30a6982fba666b8d54b53a5d2e79c9a
AlexanderLyaksa <sanchos2004@gmail.com>
2020-10-21 14:11:34 +0300

    fix options in settings

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('', include('places.urls')),
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
