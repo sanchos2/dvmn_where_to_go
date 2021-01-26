b9e3a1f1004f10b21c2ca2152bf0bb5920db462f
AlexanderLyaksa <sanchos2004@gmail.com>
2021-01-26 09:45:50 +0300

    Changed function name.

--- places/urls.py
+++ places/urls.py
@@ -1,9 +1,8 @@
-"""Urls."""
 from django.urls import path
 
 from places import get_places, views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('places/<int:pk>/', get_places.point, name='place'),
+    path('places/<int:pk>/', get_places.get_point, name='place'),
 ]
