be7bfd4e8b95fc8b6e3eee6e8d1896375b3749a9
AlexanderLyaksa <sanchos2004@gmail.com>
2021-01-26 09:29:21 +0300

    Fixed return data, added type annotation.

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,6 @@
-"""Admin module."""
 from adminsortable2.admin import SortableInlineAdminMixin
-from django.conf import settings
 from django.contrib import admin
 from django.utils.html import format_html
-from django.utils.safestring import mark_safe
 
 from places import models
 
@@ -15,16 +12,11 @@ class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     extra = 0
     readonly_fields = ['preview_image']
 
-    def preview_image(self, image):
-        """
-        Preview image.
-
-        :param image:
-        :return:
-        """
-        return format_html(mark_safe(  # noqa: S308, S703
-            f'<img src="{settings.MEDIA_URL + str(image.picture)}" height="200px" />',
-        ))
+    def preview_image(self, image) -> str:
+        """Get image preview."""
+        if image.picture:
+            return format_html('<img src="{}" height="200px" />', image.picture.url)  # noqa: P103
+        return 'Здесь будет превью, когда вы выберете файл.'
 
 
 class PlacesAdmin(admin.ModelAdmin):
