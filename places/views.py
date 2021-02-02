d216722edbaae85beec3b001becec87ed8030c32
AlexanderLyaksa <sanchos2004@gmail.com>
2021-02-02 11:00:11 +0300

    Moved get_point function to views.py

--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
 from places.models import Place
@@ -28,3 +29,21 @@ def index(request):
         )
     context = {'serialized_places': serialized_places}
     return render(request, 'index.html', context=context)
+
+
+def get_point(request: HttpRequest, pk: int) -> JsonResponse:  # noqa: WPS210
+    """Get point by id."""
+    place = get_object_or_404(Place, pk=pk)
+    images = place.images.all()
+    images_urls = [image.picture.url for image in images]
+    response = {
+        'title': place.title,
+        'imgs': images_urls,
+        'description_short': place.short_description,
+        'description_long': place.long_description,
+        'coordinates': {
+            'lng': place.lng,
+            'lat': place.lat,
+        },
+    }
+    return JsonResponse(response, json_dumps_params={'ensure_ascii': False, 'indent': 4})
