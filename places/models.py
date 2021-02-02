bb882a587b73e3c8d968fe973665aa00e336fbcf
AlexanderLyaksa <sanchos2004@gmail.com>
2021-02-02 10:23:03 +0300

    Removed type hints from models.py

--- places/models.py
+++ places/models.py
@@ -6,11 +6,11 @@ from tinymce.models import HTMLField
 class Place(models.Model):
     """Class Place."""
 
-    title: models.CharField = models.CharField('Название', max_length=200)  # noqa: WPS432
-    short_description: models.TextField = models.TextField('Краткое описание', blank=True)  # noqa: WPS432
-    long_description: HTMLField = HTMLField('Полное описание', blank=True)  # noqa: WPS432
-    lng: models.DecimalField = models.DecimalField('Долгота', max_digits=16, decimal_places=14)  # noqa: WPS432
-    lat: models.DecimalField = models.DecimalField('Широта', max_digits=16, decimal_places=14)  # noqa: WPS432
+    title = models.CharField('Название', max_length=200)  # noqa: WPS432
+    short_description = models.TextField('Краткое описание', blank=True)  # noqa: WPS432
+    long_description = HTMLField('Полное описание', blank=True)  # noqa: WPS432
+    lng = models.DecimalField('Долгота', max_digits=16, decimal_places=14)  # noqa: WPS432
+    lat = models.DecimalField('Широта', max_digits=16, decimal_places=14)  # noqa: WPS432
 
     def __str__(self) -> str:
         """Return humans string."""
@@ -27,14 +27,14 @@ class Place(models.Model):
 class Image(models.Model):
     """Class Image."""
 
-    place: models.ForeignKey = models.ForeignKey(
+    place = models.ForeignKey(
         Place,
         on_delete=models.CASCADE,
         related_name='images',
         verbose_name='Расположение',
     )
-    picture: models.ImageField = models.ImageField('Фотография')
-    picture_index: models.PositiveIntegerField = models.PositiveIntegerField(
+    picture = models.ImageField('Фотография')
+    picture_index = models.PositiveIntegerField(
         'ID фотографии',
         default=0,
         blank=False,
